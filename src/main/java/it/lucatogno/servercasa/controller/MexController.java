2f910d3bfd965a5dcbccc988a3c04e40c9ad1832
luctogno <luctogno@yahoo.it>
2014-05-08 11:53:36 +0200

    fix link statici

--- src/main/java/it/lucatogno/servercasa/controller/MexController.java
+++ src/main/java/it/lucatogno/servercasa/controller/MexController.java
@@ -2,12 +2,16 @@ package it.lucatogno.servercasa.controller;
 
 import it.lucatogno.servercasa.messagistica.ArduinoMex;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,39 +26,28 @@ public class MexController {
 	private Logger logger = Logger.getLogger(getClass());
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView vistaMessaggi() {
+	public ModelAndView vistaMessaggi(HttpServletRequest  request) {
 		RestTemplate template = new RestTemplate();
+		String url = "http://" + request.getServerName() + ":"+ request.getServerPort() + "/" + request.getContextPath();
+		logger.info(url);
 		@SuppressWarnings("unchecked")
 		List<ArduinoMex> lista = template.getForObject(
-				"http://localhost:8080/serverHome/messaggi", List.class);
+				url+"/messaggi", List.class);
 		ModelAndView view = new ModelAndView("viewMessaggi");
 		view.addObject("mexList", lista);
 		return view;
 	}
 
-	private List<ArduinoMex> prendiMessaggi() {
-		RestTemplate template = new RestTemplate();
-		@SuppressWarnings("unchecked")
-		List<LinkedHashMap> readed = template.getForObject(
-				"http://localhost:8080/serverHome/messaggi", List.class);
-		List<ArduinoMex> lista = new ArrayList<ArduinoMex>();
-		for (LinkedHashMap map : readed) {
-			lista.add(converterMex(map));
-		}
-
-		return lista;
-	}
-
-	private ArduinoMex converterMex(LinkedHashMap mappa) {
-		ArduinoMex mex = new ArduinoMex();
-		mex.setId((Long) mappa.get("id"));
-		mex.setData((Date) mappa.get("data"));
-		mex.setAuthor((String) mappa.get("author"));
-		mex.setContent((String) mappa.get("content"));
-		mex.setCommand((String) mappa.get("command"));
-		mex.setDestinatario((String) mappa.get("destinatario"));
-		return mex;
-	}
+//	private ArduinoMex converterMex(LinkedHashMap mappa) {
+//		ArduinoMex mex = new ArduinoMex();
+//		mex.setId((Long) mappa.get("id"));
+//		mex.setData((Date) mappa.get("data"));
+//		mex.setAuthor((String) mappa.get("author"));
+//		mex.setContent((String) mappa.get("content"));
+//		mex.setCommand((String) mappa.get("command"));
+//		mex.setDestinatario((String) mappa.get("destinatario"));
+//		return mex;
+//	}
 
 	@RequestMapping(value = "/sendMex", method = RequestMethod.GET)
 	public ModelAndView nuovoMessaggio() {
@@ -74,19 +67,6 @@ public class MexController {
 		view.addObject("idMex", id);
 		return view;
 
-	}
-
-	@RequestMapping(value = "/prova.do", method = RequestMethod.POST)
-	public void mememem() {
-		RestTemplate template = new RestTemplate();
-		ArduinoMex mex = new ArduinoMex();
-		mex.setAuthor("sada");
-		mex.setContent("contat");
-		mex.setDestinatario("destdass");
-		mex.setCommand("messaggio");
-		String url = "http://localhost:8080/serverHome/messaggi";
-		logger.debug("(:->)" + mex);
-		template.postForObject(url, mex, ArduinoMex.class);
 	}
 
 }
