a4584d7ec722277832af84651ad038438b96387e
luctogno <luctogno@yahoo.it>
2014-06-03 12:16:00 +0200

    Cambiato ssh

--- src/main/java/it/lucatogno/servercasa/manager/ssh/SSHConnector.java
+++ src/main/java/it/lucatogno/servercasa/manager/ssh/SSHConnector.java
@@ -1,103 +1,38 @@
 package it.lucatogno.servercasa.manager.ssh;
 
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-import org.apache.log4j.Logger;
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.connection.channel.direct.Session.Command;
+
 import org.springframework.stereotype.Service;
-
-import com.jcraft.jsch.ChannelShell;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
-import com.jcraft.jsch.UserInfo;
 
 @Service
 public class SSHConnector {
 
-	private JSch jsch;
+	public String sendCommand(String command) throws IOException {
 
-	private Session session;
-	private ChannelShell channel;
-
-	private Logger logger = Logger.getLogger(getClass());
-
-	public String connect() {
-		jsch = new JSch();
-
-		String user = "root";
-		String host = "localhost";
-		int port = 22;
-
+		SSHClient client = new SSHClient();
+		String toReturn = "";
+		client.loadKnownHosts();
+		client.connect("localhost");
 		try {
-			session = jsch.getSession(user, host, port);
-			/**
-			 * Setto la classe UserInfo per recupare la password dell'utente
-			 */
-			UserInfo ui = new SshUserInfo();
-			
-			session.setUserInfo(ui);
-
-			/**
-			 * Eseguo la connessione della sessione
-			 */
-			session.connect();
-
-			/**
-			 * Creo il channel shell
-			 */
-			channel = (ChannelShell) session.openChannel("shell");
-		} catch (JSchException e) {
-			String errorCode = "connection.failure";
-			logger.error(errorCode, e);
-			return errorCode;
+			client.authPassword("root", "bicefalo");
+			Session session = client.startSession();
+			try {
+				Command cmd = session.exec(command);
+				cmd.join(1, TimeUnit.SECONDS);
+				toReturn = cmd.getExitErrorMessage();
+			} finally {
+				session.close();
+			}
+		} finally {
+			client.disconnect();
+			client.close();
 		}
-
-		return "connected";
-	}
-
-	public String disconnect() {
-		return "disconnected";
-	}
-
-	public String receive() {
-		/**
-		 * Associo stdin e stdout
-		 */
-		channel.setInputStream(System.in);
-
-		/**
-		 * per Windows
-		 * 
-		 * channel.setInputStream(new FilterInputStream(System.in) { public
-		 * int read(byte[] b, int off, int len) throws IOException { return
-		 * in.read(b, off, (len > 1024 ? 1024 : len)); } });
-		 */
-		channel.setOutputStream(System.out);
-
-		/**
-		 * Eseguo la connessione con un timeout di 3 secondi
-		 */
-		try {
-			channel.connect(3000);
-		} catch (JSchException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return "nuovo mex";
-	}
-
-	public void send(String command) {
-		OutputStream commandAsStream = null;
-		try {
-			OutputStreamWriter stream = new OutputStreamWriter(commandAsStream, command);
-			channel.setOutputStream(commandAsStream);
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
+		return toReturn;
 	}
 
 }
